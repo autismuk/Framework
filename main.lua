0e7c183219df307ad7bca75d9177e955f1b19210
Paul Robson <paul@robsons.org.uk>
2014-07-20 21:55:09 +0100

    FSM back to event/target state only.

--- main.lua
+++ main.lua
@@ -18,6 +18,26 @@ require("framework.framework")																	-- framework.
 
 local sm = Framework:new("game.sceneManager")
 
+local FSMListener = Framework:createClass("demo.listener")
+function FSMListener:constructor(info) self:tag("fsmListener") end 
+function FSMListener:destructor() end 
+function FSMListener:onMessage(sender,name,body)
+	print(sender,name,body.currentState,body.triggerEvent,body.newState)
+end
+
+Framework:new("demo.listener")
+
+local fsm = Framework:new("system.fsm")
+print(fsm)
+
+fsm:addState("state1", {  go2 = "state2" , go1 = "state1"})
+fsm:addState("state2", {  next = "state1" })
+fsm:start("state1")
+fsm:event("go1")
+fsm:event("go2")
+fsm:event("next")
+fsm:event("go1")
+
 --- ************************************************************************************************************************************************************************
 --[[
 
