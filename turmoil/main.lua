41eefd49fc917dec89b2bc5c4c4d578a870996b2
Paul Robson <paul@robsons.org.uk>
2014-08-11 17:59:46 +0100

    changed effects

---
+++ turmoil/main.lua
@@ -0,0 +1,67 @@
+--- ************************************************************************************************************************************************************************
+---
+---				Name : 		main.lua
+---				Purpose :	Top level code "Turmoil"
+---				Created:	2 August 2014
+---				Updated:	2 August 2014
+---				Author:		Paul Robson (paul@robsons.org.uk)
+---				License:	Copyright Paul Robson (c) 2014+
+---
+--- ************************************************************************************************************************************************************************
+
+display.setStatusBar(display.HiddenStatusBar)													-- hide status bar.
+--require("main_spritetest")
+require("strict")																				-- install strict.lua to track globals etc.
+require("framework.framework")																	-- framework.
+require("utils.sound")
+require("game.status")
+require("scene.main")
+require("scene.info")
+
+Framework:new("audio.sound",																	-- create sounds object
+					{ sounds = { "dead", "move","prize","shoot","start","bomb","appear" } })
+Framework:new("game.status") 																	-- create game status object.
+
+local eFactory = Framework:new("game.enemyFactory") 											-- create an enemy factory (todo: not eventually required)
+
+local manager = Framework:new("game.manager")
+
+manager:addManagedState("start",{}, { start = "info" })											-- title page (and options)
+
+manager:addManagedState("info", 																-- Scene before starting, no exit.
+						Framework:new("scene.infoScene", { }),
+						{ start = "main", exit = "start" }) 									-- Level nn [complete] , Score, play on or go back.
+
+manager:addManagedState("main",																	-- Main play scene.
+						Framework:new("scene.mainScene"),
+						{ continue = "info", lost = "end" })									-- Continue if lives left, lost if lives zero.
+
+manager:addManagedState("end",
+						Framework:new("scene.infoScene", { message = "Game Over"}),
+						{ start = "start", exit = "start"}) 									-- Game Over, Score, go back, or go back.
+
+manager:start("info",{ })
+
+--- ************************************************************************************************************************************************************************
+--[[
+
+		Date 		Version 	Notes
+		---- 		------- 	-----
+		02-Aug-2014	0.1 		Initial version of file
+
+--]]
+--- ************************************************************************************************************************************************************************
+
+--[[
+
+1) Adaptable inbetween display.
+6.5) Collisions (enemies/player)
+
+	- on collision, lose life and destroy enemy
+	- on exit if empty then create next factory, add one life, if zero lives then exit
+	
+6.6) Outro 
+9) Title Screen.
+10) Testing esp. Admob.
+
+--]]
