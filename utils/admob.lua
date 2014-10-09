612f6ce89624fb858a3ed6b4cf9ea599f065fc8a
Paul Robson <paul@robsons.org.uk>
2014-10-09 12:23:48 +0100

    New Admob module
    Now a singleton driven by an object with very little functionality.

--- utils/admob.lua
+++ utils/admob.lua
@@ -3,7 +3,7 @@
 ---				Name : 		admob.lua
 ---				Purpose :	Admob v2 Object for Framework
 ---				Created:	20 July 2014
----				Updated:	04 Oct 2014
+---				Updated:	09 Oct 2014
 ---				Author:		Paul Robson (paul@robsons.org.uk)
 ---				License:	Copyright Paul Robson (c) 2014+
 ---
@@ -15,97 +15,107 @@ local Ads = require("ads")
 --//									Advert object. admob for iOS and Android, provides a fake if running in simulator.
 --- ************************************************************************************************************************************************************************
 
-local Advert = Framework:createClass("ads.admob")
+local AdvertManager = Framework:createClass("ads.admob.manager")
 
---//	Create an advert. Information is passed in ApplicationDescription global
---//	@info 	[table]		Constructor information (not used)
+function AdvertManager:constructor(info)
+	self.m_isDevice = system.getInfo("environment") == "device"									-- device or simulator.
+	self.m_isAndroid = system.getInfo("platformName") == "Android"								-- running on Android or iOS ?
 
-function Advert:constructor(info)
-	Advert.m_isDevice = system.getInfo("environment") == "device"								-- device or simulator.
-	Advert.m_isAndroid = system.getInfo("platformName") == "Android"							-- running on Android or iOS ?
+	if self.m_isDevice then 																	-- if it is a device.
 
-	if Advert.m_isDevice and Advert.isInitialised ~= true then 									-- is it a device, and has Ads.* not been initialised
-		Advert.isInitialised = true 															-- only initialise it once.
-		local adID 
-		if Advert.m_isAndroid then 																-- get AppID (Android)
+		if self.m_isAndroid then 																-- get AppID (Android)
 			adID = ApplicationDescription.admobIDs.android
 		else 																					-- get AppID (iOS)
 			adID = ApplicationDescription.admobIDs.ios
 		end 
 		assert(adID ~= nil,"Warning, App-ID Missing") 											-- check there actually is one.
 		Ads.init("admob",adID) 																	-- and initialise the AdMob code.
+
+		if ApplicationDescription.advertType == "interstitial" then 							-- if this application uses interstitial ads
+			Ads.load("interstitial")															-- start the preloader.
+		end
+
 	end 
+	self.m_showCount = 0 																		-- zero the show count.
+	self.m_isSuccessfullyShown = false 															-- not shown yet.
+end 
 
-	Advert.showCount = Advert.showCount or 0 													-- put default show-count value in.
+function AdvertManager:destructor()
+	while self.m_showCount > 0 do 															-- keep hiding until the advert has gone away.
+		self:hide()
+	end
+end 
 
-	--print("Ad show",Advert.showCount)
-	if Advert.showCount == 0 then 
-		if Advert.m_isDevice then 																-- if on a real device, show the banner.
+
+function AdvertManager:show()
+	assert(ApplicationDescription.advertType == "banner")										-- this is only allowed for *banners*
+
+	if self.m_showCount == 0 then 																-- is it the first show (e.g. showing from)
+		if self.m_isDevice then 																-- if on a real device, show the banner.
 			Ads.show("banner")
 		else
 			if ApplicationDescription.showDebug then  											-- otherwise, fake one.
-				Advert.m_fakeAdGroup = display.newGroup()
-				local r = display.newRect(Advert.m_fakeAdGroup,0,0,display.contentWidth,Advert.simulatorHeight)
-				r:setFillColor(0,0,0) r.strokeWidth = 2	r.anchorX = 0 r.anchorY = 0 
-				local t = display.newText(Advert.m_fakeAdGroup,"Your ad goes here ....",2,2,native.systemFont,15)
-				t.anchorX,t.anchorY = 0,0 t:setFillColor(0,1,0) Advert.m_fakeAdGroup.alpha = 0
-				self:addSingleTimer(math.random(1000,2500)/1000)								-- make it appear, pretty much randomly after a second or two
+				self.m_fakeAdGroup = display.newGroup()
+				local r = display.newRect(self.m_fakeAdGroup,0,0,display.contentWidth,self:getSimulatorHeight())
+				r:setFillColor(1,0.5,0) r.strokeWidth = 2	r.anchorX = 0 r.anchorY = 0 
+				local t = display.newText(self.m_fakeAdGroup,"Your ad goes here ....",display.contentWidth/2,
+														self:getSimulatorHeight()/2,native.systemFont,display.contentWidth/15)
+				t:setFillColor(0,0,0) 
 			end
 		end
-		Advert.isSuccessfullyShown = true 														-- setting this flag means we can now legally call getHeight()
+		self.m_isSuccessfullyShown = true 														-- setting this flag means we can now legally call getHeight()
 	else 
-		if not Advert.m_isDevice and ApplicationDescription.showDebug then  
-			Advert.m_fakeAdGroup:toFront()
+		if not self.m_isDevice and ApplicationDescription.showDebug then  						-- bring the fake advert to the front.
+			self.m_fakeAdGroup:toFront()
 		end
 	end
-	Advert.showCount = Advert.showCount + 1 													-- increment global count of shows
+	self.m_showCount = self.m_showCount + 1 													-- increment global count of shows
 end 
 
-function Advert:getDisplayObjects()
-	return { Advert.m_fakeAdGroup }
-end 
 
---//	This fakes the 'late' arrival of the actual advert, rather than it being put up straight away.
+function AdvertManager:hide()
+	self.m_showCount = self.m_showCount - 1 													-- decrement global count of shows
 
-function Advert:onTimer(tag,timerID)
-	Advert.m_fakeAdGroup.alpha = 1 																-- only one timer, which makes the fake advert visible.
-end 
-
---//	Tidy up.
-
-function Advert:destructor()
-	Advert.showCount = Advert.showCount - 1 													-- decrement global count of shows
-	-- print("Ad hide",Advert.showCount)
-	if Advert.showCount == 0 then 
-		if Advert.m_isDevice then 																-- remove the real ad or the fake one.
+	if self.m_showCount == 0 then 
+		if self.m_isDevice then 																-- remove the real ad or the fake one.
 			Ads.hide()
 		else 
 			if ApplicationDescription.showDebug then
-				Advert.m_fakeAdGroup:removeSelf() Advert.m_fakeAdGroup = nil 
+				self.m_fakeAdGroup:removeSelf() self.m_fakeAdGroup = nil 
 			end
 		end
 	end 
 end 
 
-Advert.simulatorHeight = 42 																	-- arbitrary-ish size chosen for simulator advert.
+function AdvertManager:getSimulatorHeight()
+	return 42 * display.contentHeight / 320 
+end 
 
---//	Gets the height of the banner in Corona device units. This function can only be called once the first advert is created, due to a limitation 
---//	in Corona. Therefore if you must know the height of a banner, you will have to create one and destroy one. 
---//
---//	@return 	[number]		height.
+local adManager = Framework:new("ads.admob.manager")											-- create an instance.
+AdvertManager.constructor = nil 																-- stop any more being created.
 
-function Advert:getHeight()
-	assert(Advert.isSuccessfullyShown == true,													-- Ads.getHeight() does not work until Ads.show() has been called
+
+local AdvertObject = Framework:createClass("ads.admob")
+
+function AdvertObject:constructor(info)
+	adManager:show()
+end 
+
+function AdvertObject:destructor()
+	adManager:hide()
+end
+
+function AdvertObject:getHeight()
+	assert(adManager.m_isSuccessfullyShown == true,												-- Ads.getHeight() does not work until Ads.show() has been called
 							"getHeight() called before an advert has been shown successfully")
-	if not Advert.m_isDevice then return Advert.simulatorHeight end 								-- this is an arbitrary value for the simulator
+	if not adManager.m_isDevice then return adManager:getSimulatorHeight() end 					-- this is an arbitrary value for the simulator
 	return Ads.height() 																		-- otherwise use the actual height.
 end 
 
---//	Gets the height of the banner allowing for the gap at the top of the screen
-
-function Advert:getHeightAfterGap()
+function AdvertObject:getHeightAfterGap()
 	return self:getHeight()
 end
+
 
 --- ************************************************************************************************************************************************************************
 --[[
@@ -119,6 +129,7 @@ end
 								display. Hence the counter "Advert.showCount" which tracks shows (constructors) vs hides (destructors).  Effectively when the ads is
 								on the next scene, it doesn't disappear from the screen.  Consideration should be given to making this an actual singleton ?
 		04-Oct-14 	1.2 		Moved stuff out of info into ApplicationDescription
+		09-Oct-14 	2.0 		Now a singleton and a device object
 
 --]]
 --- ************************************************************************************************************************************************************************
